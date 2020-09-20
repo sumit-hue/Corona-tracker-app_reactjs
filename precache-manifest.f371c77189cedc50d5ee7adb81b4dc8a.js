e9673e125f5d3323304945b7419281c8d5c39ff7
sumit mishra <sumit.mishra14392@gmail.com>
2020-09-21 03:08:41 +0530

    Updates

---
+++ precache-manifest.f371c77189cedc50d5ee7adb81b4dc8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a4812cd04cf9a563a314c77d6ccdb4",
+    "url": "/Corona-tracker-app_reactjs/index.html"
+  },
+  {
+    "revision": "7ac580f905dffde084c9",
+    "url": "/Corona-tracker-app_reactjs/static/css/main.69a1bd65.chunk.css"
+  },
+  {
+    "revision": "30eb15529ea4c43a972c",
+    "url": "/Corona-tracker-app_reactjs/static/js/2.4b87534b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-tracker-app_reactjs/static/js/2.4b87534b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac580f905dffde084c9",
+    "url": "/Corona-tracker-app_reactjs/static/js/main.52967082.chunk.js"
+  },
+  {
+    "revision": "1dfec812d9c731f62623",
+    "url": "/Corona-tracker-app_reactjs/static/js/runtime-main.8cd10b37.js"
+  },
+  {
+    "revision": "0b538c3be9efee94dda2c9d8161cc740",
+    "url": "/Corona-tracker-app_reactjs/static/media/image1.0b538c3b.png"
+  }
+]);
